c032c8ed44659a2d9b3bad208ddd4b1a0e76e44d
Joseph Phillips <josephephillips27@gmail.com>
2012-01-31 21:36:15 -0600

    Adding new program

---
+++ NameFormatter/NameFormatter/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace NameFormatter
+{
+    partial class FrmNameFormatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtMName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFormat4 = new System.Windows.Forms.Button();
+            this.btnFormat5 = new System.Windows.Forms.Button();
+            this.btnFormat6 = new System.Windows.Forms.Button();
+            this.btnFormat3 = new System.Windows.Forms.Button();
+            this.btnFormat2 = new System.Windows.Forms.Button();
+            this.btnFormat1 = new System.Windows.Forms.Button();
+            this.lblNameFormatter = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(87, 12);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(185, 20);
+            this.txtFName.TabIndex = 0;
+            // 
+            // txtMName
+            // 
+            this.txtMName.Location = new System.Drawing.Point(87, 38);
+            this.txtMName.Name = "txtMName";
+            this.txtMName.Size = new System.Drawing.Size(185, 20);
+            this.txtMName.TabIndex = 1;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(87, 64);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(185, 20);
+            this.txtLName.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(87, 90);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(185, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Middle Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Title";
+            // 
+            // btnFormat4
+            // 
+            this.btnFormat4.Location = new System.Drawing.Point(17, 190);
+            this.btnFormat4.Name = "btnFormat4";
+            this.btnFormat4.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat4.TabIndex = 11;
+            this.btnFormat4.Text = "Format 4";
+            this.btnFormat4.UseVisualStyleBackColor = true;
+            this.btnFormat4.Click += new System.EventHandler(this.btnFormat4_Click);
+            // 
+            // btnFormat5
+            // 
+            this.btnFormat5.Location = new System.Drawing.Point(107, 190);
+            this.btnFormat5.Name = "btnFormat5";
+            this.btnFormat5.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat5.TabIndex = 12;
+            this.btnFormat5.Text = "Format 5";
+            this.btnFormat5.UseVisualStyleBackColor = true;
+            this.btnFormat5.Click += new System.EventHandler(this.btnFormat5_Click);
+            // 
+            // btnFormat6
+            // 
+            this.btnFormat6.Location = new System.Drawing.Point(197, 190);
+            this.btnFormat6.Name = "btnFormat6";
+            this.btnFormat6.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat6.TabIndex = 13;
+            this.btnFormat6.Text = "Format 6";
+            this.btnFormat6.UseVisualStyleBackColor = true;
+            this.btnFormat6.Click += new System.EventHandler(this.btnFormat6_Click);
+            // 
+            // btnFormat3
+            // 
+            this.btnFormat3.Location = new System.Drawing.Point(197, 161);
+            this.btnFormat3.Name = "btnFormat3";
+            this.btnFormat3.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat3.TabIndex = 16;
+            this.btnFormat3.Text = "Format 3";
+            this.btnFormat3.UseVisualStyleBackColor = true;
+            this.btnFormat3.Click += new System.EventHandler(this.btnFormat3_Click);
+            // 
+            // btnFormat2
+            // 
+            this.btnFormat2.Location = new System.Drawing.Point(107, 161);
+            this.btnFormat2.Name = "btnFormat2";
+            this.btnFormat2.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat2.TabIndex = 15;
+            this.btnFormat2.Text = "Format 2";
+            this.btnFormat2.UseVisualStyleBackColor = true;
+            this.btnFormat2.Click += new System.EventHandler(this.btnFormat2_Click);
+            // 
+            // btnFormat1
+            // 
+            this.btnFormat1.Location = new System.Drawing.Point(17, 161);
+            this.btnFormat1.Name = "btnFormat1";
+            this.btnFormat1.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat1.TabIndex = 14;
+            this.btnFormat1.Text = "Format 1";
+            this.btnFormat1.UseVisualStyleBackColor = true;
+            this.btnFormat1.Click += new System.EventHandler(this.btnFormat1_Click);
+            // 
+            // lblNameFormatter
+            // 
+            this.lblNameFormatter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNameFormatter.Location = new System.Drawing.Point(17, 119);
+            this.lblNameFormatter.Name = "lblNameFormatter";
+            this.lblNameFormatter.Size = new System.Drawing.Size(255, 31);
+            this.lblNameFormatter.TabIndex = 17;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 231);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmNameFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblNameFormatter);
+            this.Controls.Add(this.btnFormat3);
+            this.Controls.Add(this.btnFormat2);
+            this.Controls.Add(this.btnFormat1);
+            this.Controls.Add(this.btnFormat6);
+            this.Controls.Add(this.btnFormat5);
+            this.Controls.Add(this.btnFormat4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtMName);
+            this.Controls.Add(this.txtFName);
+            this.Name = "FrmNameFormatter";
+            this.Text = "Name Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtMName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFormat4;
+        private System.Windows.Forms.Button btnFormat5;
+        private System.Windows.Forms.Button btnFormat6;
+        private System.Windows.Forms.Button btnFormat3;
+        private System.Windows.Forms.Button btnFormat2;
+        private System.Windows.Forms.Button btnFormat1;
+        private System.Windows.Forms.Label lblNameFormatter;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ NameFormatter/NameFormatter/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NameFormatter
+{
+    public partial class FrmNameFormatter : Form
+    {
+           //Variables for application 
+        string FName, LName, MName,Title;
+
+        public FrmNameFormatter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFormat1_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "1";
+        }
+
+        private void btnFormat2_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "2";
+        }
+
+        private void btnFormat3_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "3";
+        }
+
+        private void btnFormat4_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "4";
+        }
+
+        private void btnFormat5_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "5";
+        }
+        
+        private void btnFormat6_Click(object sender, EventArgs e)
+        {
+            lblNameFormatter.Text = "6";
+        }
+
+        private void pullVariable()
+        {
+            FName = txtFName.Text;
+            LName = txtLName.Text;
+            MName = txtMName.Text;
+            Title = txtTitle.Text;
+        }
+
+
+
+    }
+}
