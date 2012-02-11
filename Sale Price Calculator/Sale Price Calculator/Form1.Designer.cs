cc1830c79d96ec0654fc2be1199cf5aaf5ccead0
Joseph Phillips <josephephillips27@gmail.com>
2012-02-11 13:39:22 -0600

    Sale Price Calculator - Finished

---
+++ Sale Price Calculator/Sale Price Calculator/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Sale_Price_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculateSalePrice = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbxOriginalPrice = new System.Windows.Forms.TextBox();
+            this.txtbxDiscount = new System.Windows.Forms.TextBox();
+            this.lblSalePrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculateSalePrice
+            // 
+            this.btnCalculateSalePrice.Location = new System.Drawing.Point(53, 126);
+            this.btnCalculateSalePrice.Name = "btnCalculateSalePrice";
+            this.btnCalculateSalePrice.Size = new System.Drawing.Size(75, 34);
+            this.btnCalculateSalePrice.TabIndex = 3;
+            this.btnCalculateSalePrice.Text = "Calculate Sale Price";
+            this.btnCalculateSalePrice.UseVisualStyleBackColor = true;
+            this.btnCalculateSalePrice.Click += new System.EventHandler(this.btnCalculateSalePrice_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(152, 126);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 34);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sale Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter the discount percentage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter the item\'s original price:";
+            // 
+            // txtbxOriginalPrice
+            // 
+            this.txtbxOriginalPrice.Location = new System.Drawing.Point(179, 26);
+            this.txtbxOriginalPrice.Name = "txtbxOriginalPrice";
+            this.txtbxOriginalPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtbxOriginalPrice.TabIndex = 1;
+            // 
+            // txtbxDiscount
+            // 
+            this.txtbxDiscount.Location = new System.Drawing.Point(179, 54);
+            this.txtbxDiscount.Name = "txtbxDiscount";
+            this.txtbxDiscount.Size = new System.Drawing.Size(100, 20);
+            this.txtbxDiscount.TabIndex = 2;
+            // 
+            // lblSalePrice
+            // 
+            this.lblSalePrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSalePrice.Location = new System.Drawing.Point(179, 87);
+            this.lblSalePrice.Name = "lblSalePrice";
+            this.lblSalePrice.Size = new System.Drawing.Size(100, 23);
+            this.lblSalePrice.TabIndex = 0;
+            this.lblSalePrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 182);
+            this.Controls.Add(this.lblSalePrice);
+            this.Controls.Add(this.txtbxDiscount);
+            this.Controls.Add(this.txtbxOriginalPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculateSalePrice);
+            this.Name = "Form1";
+            this.Text = "Sale Price Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculateSalePrice;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbxOriginalPrice;
+        private System.Windows.Forms.TextBox txtbxDiscount;
+        private System.Windows.Forms.Label lblSalePrice;
+    }
+}
+
---
+++ Sale Price Calculator/Sale Price Calculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sale_Price_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateSalePrice_Click(object sender, EventArgs e)
+        {
+            //Declare Variables
+            decimal OriginalPrice;
+            decimal DiscountPercentage;
+            decimal DiscountAmount;
+            decimal SalesPrice;
+
+            //Assign input to variables
+            OriginalPrice = decimal.Parse(txtbxOriginalPrice.Text);
+            DiscountPercentage = decimal.Parse(txtbxDiscount.Text);
+
+            //Covert DiscountPercentage
+            DiscountPercentage = DiscountPercentage / 100;
+            
+            //Calculate the Discount Amount
+            DiscountAmount = OriginalPrice * DiscountPercentage;
+
+            //Calculate Sales Price
+            SalesPrice = OriginalPrice - DiscountAmount;
+
+            //Dispaly Sales Price
+            lblSalePrice.Text = SalesPrice.ToString("c");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close application
+            this.Close();
+        }
+    }
+}
