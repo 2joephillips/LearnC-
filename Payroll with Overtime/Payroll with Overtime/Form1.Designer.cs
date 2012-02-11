fe430cedc52e3e32fe5fbb73046a98915efe74b9
Joseph Phillips <josephephillips27@gmail.com>
2012-02-11 15:46:28 -0600

    Able to calculate OT pay

--- Payroll with Overtime/Payroll with Overtime/Form1.Designer.cs
+++ Payroll with Overtime/Payroll with Overtime/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.exitButton.TabIndex = 26;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // clearButton
             // 
@@ -56,6 +57,7 @@
             this.clearButton.TabIndex = 25;
             this.clearButton.Text = "Clear";
             this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // calculateButton
             // 
@@ -65,6 +67,7 @@
             this.calculateButton.TabIndex = 24;
             this.calculateButton.Text = "Calculate Gross Pay";
             this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
             // grossPayLabel
             // 
--- Payroll with Overtime/Payroll with Overtime/Form1.cs
+++ Payroll with Overtime/Payroll with Overtime/Form1.cs
@@ -15,5 +15,72 @@ namespace Payroll_with_Overtime
         {
             InitializeComponent();
         }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close Application
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Clear input and output controls
+            hoursWorkedTextBox.Text = "";
+            hourlyPayRateTextBox.Text = "";
+            grossPayLabel.Text = "";
+
+            //Reset focus 
+            hoursWorkedTextBox.Focus();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Name constants
+                const decimal BASE_HOURS = 40m;
+                const decimal OT_MULTIPLIER = 1.5m;
+
+                //Local variables
+                decimal hoursWorked;
+                decimal hourlyPayRate;
+                decimal basePay;
+                decimal overtimeHours;
+                decimal overtimePay;
+                decimal grossPay;
+
+                //Get the hours worked and hourly pay rate.
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+
+                //Determine the gross pay.
+                if (hoursWorked > BASE_HOURS)
+                {
+                    //Calculate the base pay 
+                    basePay = hourlyPayRate * BASE_HOURS;
+
+                    //Calculate the number of overtime hours.
+                    overtimeHours = hoursWorked - BASE_HOURS;
+
+                    //Calculate overtime pay
+                    overtimePay = overtimeHours * OT_MULTIPLIER;
+
+                    //Calculate the gross pay.
+                    grossPay = basePay + overtimePay;
+                }
+                else
+                {
+                    //Calculate gross pay.
+                    grossPay = hoursWorked * hourlyPayRate;
+                }
+
+                //Display the gross pay.
+                grossPayLabel.Text = grossPay.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
