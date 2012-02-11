8bfb5ebcaf887c3d50fb6b91d13c766b29dde233
Joseph Phillips <josephephillips27@gmail.com>
2012-02-11 14:39:23 -0600

    Test Average Updated

---
+++ Test Average/Test Average/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Test_Average
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbxTest1 = new System.Windows.Forms.TextBox();
+            this.txtbxTest2 = new System.Windows.Forms.TextBox();
+            this.txtbxTest3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Test 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Test 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Test 3:";
+            // 
+            // txtbxTest1
+            // 
+            this.txtbxTest1.Location = new System.Drawing.Point(119, 12);
+            this.txtbxTest1.Name = "txtbxTest1";
+            this.txtbxTest1.Size = new System.Drawing.Size(100, 20);
+            this.txtbxTest1.TabIndex = 3;
+            // 
+            // txtbxTest2
+            // 
+            this.txtbxTest2.Location = new System.Drawing.Point(119, 48);
+            this.txtbxTest2.Name = "txtbxTest2";
+            this.txtbxTest2.Size = new System.Drawing.Size(100, 20);
+            this.txtbxTest2.TabIndex = 4;
+            // 
+            // txtbxTest3
+            // 
+            this.txtbxTest3.Location = new System.Drawing.Point(119, 84);
+            this.txtbxTest3.Name = "txtbxTest3";
+            this.txtbxTest3.Size = new System.Drawing.Size(100, 20);
+            this.txtbxTest3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Average Test Score:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAverage.Location = new System.Drawing.Point(109, 118);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(141, 23);
+            this.lblAverage.TabIndex = 7;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(9, 178);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 37);
+            this.btnAverage.TabIndex = 8;
+            this.btnAverage.Text = "Calculate Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(92, 178);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 37);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(175, 178);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 37);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 268);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbxTest3);
+            this.Controls.Add(this.txtbxTest2);
+            this.Controls.Add(this.txtbxTest1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Test Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbxTest1;
+        private System.Windows.Forms.TextBox txtbxTest2;
+        private System.Windows.Forms.TextBox txtbxTest3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Test Average/Test Average/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declare Variables
+                double test1, test2, test3, average;
+                test1 = double.Parse(txtbxTest1.Text);
+                test2 = double.Parse(txtbxTest2.Text);
+                test3 = double.Parse(txtbxTest3.Text);
+
+                //Calculate the average
+                average = (test1 + test2 + test3) / 3.0;
+
+                //Display the average test score.
+                // the output rounded to 1 decimal point
+                lblAverage.Text = average.ToString("n1");
+            }
+            catch (Exception ex)
+            {
+                //Display the default error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clear input and output controls
+            lblAverage.Text = "";
+            txtbxTest1.Text = "";
+            txtbxTest2.Text = "";
+            txtbxTest3.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close application
+            this.Close();
+        }
+    }
+}
