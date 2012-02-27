39c526196fad2c1e0a79d6b1ecaffdff6d631d97
Joseph Phillips <josephephillips27@gmail.com>
2012-02-26 20:56:58 -0600

    created the friend file program

---
+++ Friend File/Friend File/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Friend_File
+{
+    partial class FrmFriendFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWriteName = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnWriteName
+            // 
+            this.btnWriteName.Location = new System.Drawing.Point(250, 102);
+            this.btnWriteName.Name = "btnWriteName";
+            this.btnWriteName.Size = new System.Drawing.Size(75, 23);
+            this.btnWriteName.TabIndex = 0;
+            this.btnWriteName.Text = "Write Name";
+            this.btnWriteName.UseVisualStyleBackColor = true;
+            this.btnWriteName.Click += new System.EventHandler(this.btnWriteName_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(331, 102);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a Friend\'s Name:";
+            // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(133, 40);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(261, 20);
+            this.txtbxName.TabIndex = 3;
+            // 
+            // FrmFriendFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 171);
+            this.Controls.Add(this.txtbxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnWriteName);
+            this.Name = "FrmFriendFile";
+            this.Text = "Friend File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWriteName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbxName;
+    }
+}
+
---
+++ Friend File/Friend File/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Friend_File
+{
+    public partial class FrmFriendFile : Form
+    {
+        public FrmFriendFile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close Application
+            this.Close();
+        }
+
+        private void btnWriteName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declare a Streamwriter Variable
+                StreamWriter outputfile;
+
+                //Create a file and get a StreamWriter Object
+                outputfile = File.CreateText("Friend.txt");
+
+                //Write Name to file
+                outputfile.WriteLine(txtbxName.Text);
+
+                //Close file
+                outputfile.Close();
+
+                //Show item was added to file
+                MessageBox.Show("The name was written.");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+       
+    }
+}
