61839a1ffe31365ab94d4a274489abfa6b9376c8
Joseph Phillips <josephephillips27@gmail.com>
2012-02-26 20:24:24 -0600

    - Speed Converter program.

---
+++ Speed Converter/Speed Converter/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Speed_Converter
+{
+    partial class frmSpeedConvertor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxOutput = new System.Windows.Forms.ListBox();
+            this.btnDisplaySpeed = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxOutput
+            // 
+            this.lstbxOutput.FormattingEnabled = true;
+            this.lstbxOutput.Location = new System.Drawing.Point(13, 13);
+            this.lstbxOutput.Name = "lstbxOutput";
+            this.lstbxOutput.Size = new System.Drawing.Size(259, 173);
+            this.lstbxOutput.TabIndex = 0;
+            // 
+            // btnDisplaySpeed
+            // 
+            this.btnDisplaySpeed.Location = new System.Drawing.Point(33, 202);
+            this.btnDisplaySpeed.Name = "btnDisplaySpeed";
+            this.btnDisplaySpeed.Size = new System.Drawing.Size(75, 48);
+            this.btnDisplaySpeed.TabIndex = 1;
+            this.btnDisplaySpeed.Text = "Display Speed";
+            this.btnDisplaySpeed.UseVisualStyleBackColor = true;
+            this.btnDisplaySpeed.Click += new System.EventHandler(this.btnDisplaySpeed_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(114, 202);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 48);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmSpeedConvertor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDisplaySpeed);
+            this.Controls.Add(this.lstbxOutput);
+            this.Name = "frmSpeedConvertor";
+            this.Text = "Speed Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxOutput;
+        private System.Windows.Forms.Button btnDisplaySpeed;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Speed Converter/Speed Converter/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Speed_Converter
+{
+    public partial class frmSpeedConvertor : Form
+    {
+        public frmSpeedConvertor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close Application
+            this.Close();
+        }
+
+        private void btnDisplaySpeed_Click(object sender, EventArgs e)
+        {
+            //Declare Constants
+            const double CONVERSION_FACTOR = 0.6214;
+            const int START_SPEED = 60;
+            const int FINISH_SPEED = 130;
+            const int INTERVAL = 10;
+
+            //Variables 
+            int     kph;
+            double  mph;
+
+            for ( kph = START_SPEED; kph < FINISH_SPEED; kph += INTERVAL)
+            {
+                //Calculate miles per hour
+                mph = kph * CONVERSION_FACTOR;
+
+                lstbxOutput.Items.Add(kph + " KPH is the same as " + mph + " MPH");
+            }
+
+        }
+    }
+}
