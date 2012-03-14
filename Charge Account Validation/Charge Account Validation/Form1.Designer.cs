3835da2efd013bfcfbc3b852e28a9a6b3e63b238
Joseph Phillips <josephephillips27@gmail.com>
2012-03-13 21:01:09 -0500

    Charge Account Validation - Finished product

---
+++ Charge Account Validation/Charge Account Validation/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Charge_Account_Validation
+{
+    partial class chargeAccountValidation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.txtbxChargeAccount = new System.Windows.Forms.TextBox();
+            this.btnValidateAccountNumber = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(36, 22);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(179, 13);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "Enter 7 digit charge account number";
+            // 
+            // txtbxChargeAccount
+            // 
+            this.txtbxChargeAccount.Location = new System.Drawing.Point(47, 50);
+            this.txtbxChargeAccount.Name = "txtbxChargeAccount";
+            this.txtbxChargeAccount.Size = new System.Drawing.Size(100, 20);
+            this.txtbxChargeAccount.TabIndex = 1;
+            // 
+            // btnValidateAccountNumber
+            // 
+            this.btnValidateAccountNumber.Location = new System.Drawing.Point(39, 89);
+            this.btnValidateAccountNumber.Name = "btnValidateAccountNumber";
+            this.btnValidateAccountNumber.Size = new System.Drawing.Size(75, 23);
+            this.btnValidateAccountNumber.TabIndex = 2;
+            this.btnValidateAccountNumber.Text = "Validate";
+            this.btnValidateAccountNumber.UseVisualStyleBackColor = true;
+            this.btnValidateAccountNumber.Click += new System.EventHandler(this.btnValidateAccountNumber_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(146, 89);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chargeAccountValidation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 136);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnValidateAccountNumber);
+            this.Controls.Add(this.txtbxChargeAccount);
+            this.Controls.Add(this.lbl);
+            this.Name = "chargeAccountValidation";
+            this.Text = "Charge Account Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.TextBox txtbxChargeAccount;
+        private System.Windows.Forms.Button btnValidateAccountNumber;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Charge Account Validation/Charge Account Validation/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Charge_Account_Validation
+{
+    public partial class chargeAccountValidation : Form
+    {
+        public chargeAccountValidation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Close Form.
+            this.Close();
+        }
+
+        private void btnValidateAccountNumber_Click(object sender, EventArgs e)
+        {
+            //Declare Variable and intialize variable for users input and return account number.
+            string input = txtbxChargeAccount.Text;
+            int accountNumber = 0;
+
+            //Declare Variable for Account Number file.
+            string fileName = "ChargeAccounts.txt";
+
+            //Create list of AccountNumbers
+            List<int> accountNumbers = new List<int>();
+
+            if (ValidateInput(input, ref accountNumber))
+            {
+                //Call method to create list of Account number.
+                CreateListOfAccountNumbers(fileName, ref accountNumbers);
+
+                //Check if input matches active account numbers and return message.
+                if (ValidAccountNumber(accountNumber, accountNumbers))
+                {
+                    MessageBox.Show("Input is a valid account number.");
+                }
+                else
+                {
+                    MessageBox.Show("Input is not a valid account number.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a 7 digit account number");
+            }
+        }
+
+        private bool ValidAccountNumber(int accountNumber, List<int> accountNumbers)
+        {
+            bool validation = false;
+            foreach (int activeAccount in accountNumbers)
+            {
+                if (activeAccount == accountNumber)
+                {
+                    validation = true;
+                }
+            }
+
+            return validation;
+        }
+
+        private void CreateListOfAccountNumbers(string fileName, ref List<int> AccountNumbers)
+        {
+            //Open ChargeAccountsList.txt File
+            StreamReader InputFile = File.OpenText(fileName);
+
+            //Read Account numbers from file and load into list as int.
+            try
+            {
+                while (!InputFile.EndOfStream)
+                {
+                    AccountNumbers.Add(int.Parse(InputFile.ReadLine()));
+                }
+            }
+            catch (Exception ex)
+            {
+                //Error Message.
+                MessageBox.Show(ex.Message);
+            }
+
+            InputFile.Close();
+        }
+
+        private bool ValidateInput(string input, ref int accountNumber)
+        {
+            bool validation = false;
+            if (input.Length == 7 && (int.TryParse(input, out accountNumber )))
+            {
+                validation = true;
+            }
+
+            return validation;
+        }
+
+               
+    }
+}
