b52bea16b8ecf8268dd664d5d924f4f15ce7f75f
joseph Phillips <josephephillips27@gmail.com>
2012-03-19 20:42:38 -0500

    completed wordcounter.

---
+++ Word Counter/Word Counter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Word_Counter
+{
+    partial class frmWordCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbxInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Sentence and Press Go:";
+            // 
+            // txtbxInput
+            // 
+            this.txtbxInput.Location = new System.Drawing.Point(16, 30);
+            this.txtbxInput.Name = "txtbxInput";
+            this.txtbxInput.Size = new System.Drawing.Size(453, 20);
+            this.txtbxInput.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(16, 75);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(98, 75);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmWordCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 113);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtbxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "frmWordCounter";
+            this.Text = "Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbxInput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Word Counter/Word Counter/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Word_Counter
+{
+    public partial class frmWordCounter : Form
+    {
+        public frmWordCounter()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Closes the form 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close Application
+            this.Close();
+        }
+
+        /// <summary>
+        /// When pressed calculates the number of words within a sentence.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGo_Click(object sender, EventArgs e)
+        {   
+            //Create and initalize the variable for the textbox
+            string input = txtbxInput.Text;
+
+            //Calls the method response to display the response based on 
+            //what is entered into the textbox.
+            MessageBox.Show(Response(input));
+        }
+
+        /// <summary>
+        /// Creates a string for the btnGo_Click method.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private string Response(string input)
+        {
+            //Checks to make sure that the textbox has inforamation in it.
+            //If it is null or empty returns error message to user. 
+            if(!string.IsNullOrWhiteSpace(input))    
+            {
+                //Creates a variable for the number of words in the sentence. 
+               string numberOfWords = CountNumberOfWords(input);
+                //Creates the return string.
+               return "There are a total of " + numberOfWords + " word(s) in this sentence."; 
+            }
+            else
+            {
+                //Creates the return mesage for empty or null input.
+                return "Enter sentence then press Go.";
+            }    
+        }
+
+        /// <summary>
+        ///Count the number of words. To do this, I count the number of spaces within the 
+        ///input string. 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private string CountNumberOfWords(string input)
+        {
+            //Create variable for the tokens
+            string[] tokens = input.Split(null);
+            //Return the lenth which will be the number of words as a string.
+            return tokens.Length.ToString();
+
+        }
+    }
+}
